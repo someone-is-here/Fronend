43d8e59df1f8914108e66559f9ecb6622e43db71
Tanusha <tanusha20020@tut.by>
2023-05-12 00:32:28 +0300

    update sidenav

--- streaming/js/sidenav.js
+++ streaming/js/sidenav.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-app.js";
-import { getDatabase, ref, set, update } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-database.js";
+import { getDatabase, ref, set, update, get } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-database.js";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-auth.js";
 
 
@@ -38,12 +38,38 @@ function menuBaseTemplate(){
   <a href="login.html" class="sidenav-link">Login</a>
   <a href="register.html" class="sidenav-link">Register</a>`;
 }
+function generateArtistFunctionality() {
+  return `      <a href="add_instrument.html" class="sidenav-link">Add instrument</a>
+                <a href="create_track.html" class="sidenav-link">Add track</a>
+                <a href="add_genre.html" class="sidenav-link">Add genre</a>
+                <a href="add_label.html" class="sidenav-link">Add label</a>`;
+}
+function generateUserFunctionality() {
+  return `<a href="change_subscription.html" class="sidenav-link">Subscription</a>`;
+}
 const user = auth.currentUser;
 onAuthStateChanged(auth, (user) => {
   if (user) {
     const uid = user.uid;
     console.log(user.email)
     document.getElementById("ul__sidenav-list").innerHTML += menuTemplateLogin(user.email);
+
+    const dbRef = ref(getDatabase());
+    get(child(dbRef, `users/${uid}`)).then((snapshot) => {
+  if (snapshot.exists()) {
+    console.log(snapshot.val());
+  } else {
+    console.log("No data available");
+  }
+}).catch((error) => {
+  console.error(error);
+});
+
+    if (user.role === "2"){
+        document.getElementById("ul__sidenav-list").innerHTML += generateArtistFunctionality();
+    }else if(user.role === "3"){
+document.getElementById("ul__sidenav-list").innerHTML += generateUserFunctionality();
+    }
   } else {
     document.getElementById("ul__sidenav-list").innerHTML += menuBaseTemplate();
   }
