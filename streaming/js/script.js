001c220f77fbc67e47f5b0de70e45aa78bd9068e
Tanusha <tanusha20020@tut.by>
2023-05-17 21:01:06 +0300

    fix subscription styling

--- streaming/js/script.js
+++ streaming/js/script.js
@@ -1,15 +1,16 @@
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
-function openNav() {
-  if(document.getElementById("sidenav").style.width == "10rem"){
-    closeNav()
-  }else {
-    document.getElementById("sidenav").style.width = "10rem";
-    document.body.style.opacity = "0.9";
-  }
+const elAll = document.getElementsByClassName("small__heart");
+for (let el of elAll){
+    el.addEventListener('click',function(event){
+        el = event.currentTarget.parentNode.parentNode.getElementsByClassName("span__hearts-amount")[0];
+        let el_content = +el.innerHTML;
+        if(event.currentTarget.classList.contains("small__heart-red")){
+            event.currentTarget.classList.remove("small__heart-red");
+            el.innerHTML = el_content - 1;
+        } else {
+            event.currentTarget.classList.add("small__heart-red");
+            el.innerHTML = el_content + 1;
+        }
+    });
 }
 
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
-function closeNav() {
-  document.getElementById("sidenav").style.width = "0";
-      document.body.style.opacity = "1";
-}
+
